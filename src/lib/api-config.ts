ac7d5a9a87b833e08ed789fbbe96b76290d421b0
Mikael Daskalou <mikael.daskalou@hotmail.com>
2026-03-03 23:06:44 +0100

    löst bugg

--- src/lib/api-config.ts
+++ src/lib/api-config.ts
@@ -1,3 +1,6 @@
+import { BACKEND_API_URL } from '@/lib/constants';
+
+export const API_BASE_URL = BACKEND_API_URL;
 
 export const getHeaders = (token?: string) => {
     const headers: HeadersInit = {
